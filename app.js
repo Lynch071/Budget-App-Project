fbb45089847e6548878bcae2f6c25828faadb84b
Lynch071 <69902159+Lynch071@users.noreply.github.com>
2020-10-14 14:29:27 +0200

    implementing modules

---
+++ app.js
@@ -0,0 +1,20 @@
+var budgetController = (function() {
+
+
+
+
+})();
+
+var UIController = (function() {
+
+
+
+
+})();
+
+var controller = (function(budgetCtrl,UICtrl) {
+
+
+
+
+})(budgetController, UIController);
