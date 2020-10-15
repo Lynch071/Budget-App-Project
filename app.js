576f911028a67cc20107cd50dcb274b561f9ed13
Lynch071 <69902159+Lynch071@users.noreply.github.com>
2020-10-15 12:35:58 +0200

    Structure the app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
+
 
 
 
 
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
 
 
@@ -12,9 +15,22 @@ var UIController = (function() {
 
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl,UICtrl) {
 
+    document.querySelector('.add__btn').addEventListener('click', function() {
+        
+        // 1. Get the filed input data
 
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+        
+    });
 
 
 })(budgetController, UIController);
