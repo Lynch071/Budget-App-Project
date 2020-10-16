f18e6019616a0d1afe4fa170a057d3459fcd064f
Lynch071 <69902159+Lynch071@users.noreply.github.com>
2020-10-16 11:02:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,9 @@ var controller = (function(budgetCtrl,UICtrl) {
     var ctrlAddItem = function() {
         
         // 1. Get the field input data
-
+        var input = UICtrl.getinput();
+        console.log(input);
+        
         // 2. Add the item to the budget controller
 
         // 3. Add the item to the UI
