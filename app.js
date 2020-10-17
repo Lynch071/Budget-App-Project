2aa343827a04f6af0fa16271399faf68682a9746
Lynch071 <69902159+Lynch071@users.noreply.github.com>
2020-10-17 16:04:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -159,18 +159,21 @@ var controller = (function(budgetCtrl,UICtrl) {
         var input, newItem;
         // 1. Get the field input data
         input = UICtrl.getinput();
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            
+            // 2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-        // 2. Add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+            // 3. Add the item to the UI
+            UICtrl.addListItem(newItem, input.type);
 
-        // 3. Add the item to the UI
-        UICtrl.addListItem(newItem, input.type);
+            // 4. Clear the fields
+            UICtrl.clearFields();
 
-        // 4. Clear the fields
-        UICtrl.clearFields();
+            // Calculate and update budget
+            updateBudget();
 
-        // Calculate and update budget
-        updateBudget();
+        }
 
     };
 
