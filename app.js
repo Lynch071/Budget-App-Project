485486f3e8f97043ef06397aaf9243b6b39eea86
Lynch071 <69902159+Lynch071@users.noreply.github.com>
2020-10-17 12:41:29 +0200

    Update app.js
    add html

--- app.js
+++ app.js
@@ -76,7 +76,8 @@ var UIController = (function() {
         addListItem: function(obj, type) {
 
             // Create HTML string with placeholder text
-            
+
+            '<div class="item clearfix" id="income-0"><div class="item__description">Salary</div><div class="right clearfix"><div class="item__value">+ 2,100.00</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
 
             // Replace the placeholder text with some actual data
 
