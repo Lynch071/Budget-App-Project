650aabc6e94ce67a824568b8e95515dea801df10
Lynch071 <69902159+Lynch071@users.noreply.github.com>
2020-10-17 12:36:42 +0200

    Update app.js
    adding todo list

--- app.js
+++ app.js
@@ -73,6 +73,19 @@ var UIController = (function() {
             };
         },
 
+        addListItem: function(obj, type) {
+
+            // Create HTML string with placeholder text
+            
+
+            // Replace the placeholder text with some actual data
+
+
+            // Insert the HTML into the DOM
+
+
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
