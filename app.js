fa3ad5fb18b39d47e2d31e5d7be1b0ba3f9751f1
Lynch071 <69902159+Lynch071@users.noreply.github.com>
2020-10-17 16:42:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ var budgetController = (function() {
             data.budget = data.totals.inc - data.totals.exp;
 
             // Calculate percentage of income that we spent
-            data.percentage = Math.round(data.totals.exp / data.totals.inc) *100;
+            data.percentage = Math.round((data.totals.exp / data.totals.inc) *100);
         }
 
     };
