20ece89f0c77e078c8168f6d81e864cca072bd55
Lynch071 <69902159+Lynch071@users.noreply.github.com>
2020-10-18 16:00:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ var budgetController = (function() {
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
+    };
+
+    Expense.prototype.calcPercentage = function(totalIncome) {
+
+        this.percentage = Math.round((this.value / totalIncome) * 100);
+
     };
 
     var Income = function(id, description, value) {
@@ -90,6 +97,8 @@ var budgetController = (function() {
                 data.percentage = -1;
             }
         },
+
+
 
         getBudget: function() {
 
